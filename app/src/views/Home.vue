7486513687e707b1628ad9c791e7cd03c83c183d
Qbot <kubacembrzynski97@o2.pl>
2020-03-06 19:29:43 +0100

    Added vue

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <LeftMenuListComponent/>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+import LeftMenuListComponent from '../components/LeftMenuListComponent'
+  export default {
+    props: {
+      source: String,
+    },
+    components: {
+      LeftMenuListComponent
+    },
+    data: () => ({
+      drawer: null,
+    }),
   }
-}
 </script>

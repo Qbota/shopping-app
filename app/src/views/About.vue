04b383fbc5e5ec9288938263d4c8bcd8b810b010
Qbot <kubacembrzynski97@o2.pl>
2020-05-08 19:57:35 +0200

    Added drag and drop component

---
+++ app/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <DragComponent/>
+  </div>
+</template>
+<script>
+  import DragComponent from "@/components/DragComponent";
+  export default {
+    components: {DragComponent}
+  }
+</script>
